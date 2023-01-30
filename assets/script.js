ab04c057101fee3a10da26513bcd0ea24db56d29
allisonnault <allisonrnault@gmail.com>
2023-01-29 18:06:30 -0600

    switching to a for loop

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,47 @@
-var card1 = document.querySelector(".one");
-var card2 = document.querySelector(".two");
-var card3 = document.querySelector(".three");
-var card4 = document.querySelector(".four");
-var card5 = document.querySelector(".five");
+var card1 = document.querySelector(".start");
+var card2 = document.querySelector(".question");
 
+var Questions = [
+    {question: "How would you access an HTML element by a specified classname?", 
+    choices:{
+        A: "A. getElementByClass",
+        B: "B. getElementById",
+        C: "C. getElementbyTagName",
+        D: "D. querySelector",
+    }},
+    {question: "Which is NOT a data type in Javascript",
+    choices:{
+        A: "A. Array",
+        B: "B. Function",
+        C: "C. Boolean",
+        D: "D. Object",
+    }},
+    {question: "What symbol(s) would you use to express strict equalitiy?",
+    choices:{
+        A: "A. !== ",
+        B: "B. == ",
+        C: "C. => ",
+        D: "D. === ",
+    }}
+   ]
+
+// when the page loads, only card 1 will be visible
 function loadQuiz() {
     card1.setAttribute("class", "visible");
 }
 
 var startQuiz = document.querySelector("#start");
 // add event listener to start quiz
-startQuiz.addEventListener("click", function() {
-    if(startQuiz) {
-// set timer
-    setTime ();
+startQuiz.addEventListener("click", function () {
+    if (startQuiz) {
+        // set timer
+        setTime();
 
-    card1.removeAttribute("class", "visible");
-    card1.setAttribute("class", "card");
-    card2.setAttribute("class", "visible");
-// add carousel function to switch cards
+        card1.removeAttribute("class", "visible");
+        card1.setAttribute("class", "card");
+        card2.setAttribute("class", "visible");
+
+// create a for loop to load in questions and choices
 
     }
 })
@@ -26,51 +49,18 @@ startQuiz.addEventListener("click", function() {
 var secondsLeft = 300;
 
 function setTime() {
-  // Sets interval in variable
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    console.log(secondsLeft);
+    // Sets interval in variable
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        console.log(secondsLeft);
 
-    if(secondsLeft === 0) {
-      // Stops execution of action at set interval
-      clearInterval(timerInterval);
-    //   sendMessage();
-    // END QUIZ AND SHOW SCORE
-    // define below
-    }
-  }, 1000);
+        if (secondsLeft === 0) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+            //   sendMessage();
+            // END QUIZ AND SHOW SCORE
+            // define below
+        }
+    }, 1000);
 }
 
-var next = document.querySelector(".next");
-var prev = document.querySelector(".prev");
-var index = 0;
-var currentCard;
-var cards = document.querySelectorAll(".card");
-
-function navigate(direction) {
-    index = index + direction;
-    if (index < 0) { 
-      index = cards.length - 1; 
-    } else if (index > cards.length - 1) { 
-      index = 0;
-    }
-    currentCard = cards[index];
-    if (currentCard) {
-        setAttribute("class", "visible");    
-    } else {
-        removeAttribute("class", "visible");
-        setAttribute("class", "card");
-    }
-  }
-  
-  next.addEventListener("click", function(event) {
-    event.stopPropagation();
-    navigate(1);
-  });
-
-  prev.addEventListener("click", function(event) {
-  event.stopPropagation();
-  navigate(-1);
-});
-
-navigate(0);
