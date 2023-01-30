5b6dea5f1c4210ef2a339bdd8554ea4fd08de7af
allisonnault <allisonrnault@gmail.com>
2023-01-30 15:32:15 -0600

    modified javascript

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,24 @@
 var card1 = document.querySelector(".start");
 var card2 = document.querySelector(".question");
+var Question = document.querySelector("#question");
+var timerSpan = document.querySelector("#timer");
+var choiceList = document.querySelector("#choices");
+var startQuiz = document.querySelector("#start");
 
-var Questions = [
-    {question: "How would you access an HTML element by a specified classname?", 
-    choices:{
-        A: "A. getElementByClass",
-        B: "B. getElementById",
-        C: "C. getElementbyTagName",
-        D: "D. querySelector",
-    }},
-    {question: "Which is NOT a data type in Javascript",
-    choices:{
-        A: "A. Array",
-        B: "B. Function",
-        C: "C. Boolean",
-        D: "D. Object",
-    }},
-    {question: "What symbol(s) would you use to express strict equalitiy?",
-    choices:{
-        A: "A. !== ",
-        B: "B. == ",
-        C: "C. => ",
-        D: "D. === ",
-    }}
-   ]
+var quizQuestions = ["How would you access an HTML element by a specified classname?", "Which is NOT a data type in Javascript", "What symbol(s) would you use to express strict equalitiy?"];
+
+var choice1 = ["getElementByClass", "getElementById", "getElementbyTagName", "querySelector"];
+var choice2 = ["Array", "unction", "Boolean", "Object"];
+var choice3 = [" !== ", " == ", " => ", " === "];
+
+var quizChoices = [choice1, choice2, choice3];
+
 
 // when the page loads, only card 1 will be visible
 function loadQuiz() {
     card1.setAttribute("class", "visible");
 }
 
-var startQuiz = document.querySelector("#start");
 // add event listener to start quiz
 startQuiz.addEventListener("click", function () {
     if (startQuiz) {
@@ -41,18 +29,17 @@ startQuiz.addEventListener("click", function () {
         card1.setAttribute("class", "card");
         card2.setAttribute("class", "visible");
 
-// create a for loop to load in questions and choices
-
+        populateQuiz();
     }
 })
 
-var secondsLeft = 300;
+var secondsLeft = 100;
 
 function setTime() {
     // Sets interval in variable
     var timerInterval = setInterval(function () {
         secondsLeft--;
-        console.log(secondsLeft);
+        timerSpan.textContent = secondsLeft;
 
         if (secondsLeft === 0) {
             // Stops execution of action at set interval
@@ -64,3 +51,22 @@ function setTime() {
     }, 1000);
 }
 
+function populateQuiz() {
+    // questions
+    for (let i = 0; i < quizQuestions.length; i++) {
+        var currentQuestion = quizQuestions[i];
+
+        var h1 = document.createElement("h1");
+        h1.textContent = currentQuestion;
+        Question.appendChild(h1);
+    }
+    // choices
+    for (let i = 0; i < quizChoices.length; i++) {
+        var currentChoices = quizChoices[i];
+        currentChoices.forEach((item) => {
+            let li = document.createElement("li");
+            li.textContent = item;
+            choiceList.appendChild(li);
+        });
+    }
+}
