e1d5cd461fd8f4247cef0c6f979ca24f9dcdc64e
allisonnault <allisonrnault@gmail.com>
2023-01-31 16:16:12 -0600

    started on local storage

---
+++ site pages/assets/scriptHS.js
@@ -0,0 +1,20 @@
+var scoreCard = document.querySelector('#score-card')
+
+var topScores = [];
+
+function listScores() {
+    for (var i = 0; i < 5; i++) {
+        var scoreLi = topScore[i];
+
+        var li = document.createElement("li");
+        li.textContent = scoreLi;
+        scoreCard.appendChild(li);
+    }
+}
+
+function init() {
+    var storedScores = localStorage.getItem("score");
+console.log(storedScores);
+}
+
+init();
