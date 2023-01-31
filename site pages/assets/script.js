6503c1f72ba0646c79b24d2ece3582c326cde955
allisonnault <allisonrnault@gmail.com>
2023-01-31 15:15:44 -0600

    fixed end quiz issue

--- site pages/assets/script.js
+++ site pages/assets/script.js
@@ -6,9 +6,9 @@ var timerSpan = document.querySelector("#timer");
 var choiceList = document.querySelector("#choices");
 var startBtn = document.querySelector("#start");
 var spanMessage = document.querySelector("#message");
-
-var questionIndex = 0
-var secondsLeft = 5;
+var score = 0;
+var questionIndex = 0;
+var secondsLeft = 15;
 var questionList = [
     {
         question: "How would you access an HTML element by a specified classname?",
@@ -55,14 +55,16 @@ function setTime() {
         secondsLeft--;
         timerSpan.textContent = secondsLeft + " seconds left";
 
-        if (secondsLeft === 0) {
+        if (secondsLeft <= 0) {
             // Stops execution of action at set interval
+            secondsLeft = 0;
             clearInterval(timerInterval);
             card2.removeAttribute("class", "visible");
             card2.setAttribute("class", "hidden");
             card3.setAttribute("class", "visible");
 
         }
+        console.log(questionIndex);
     }, 1000);
 }
 
@@ -82,8 +84,16 @@ function populateQuiz() {
 
 function nextQuestion() {
     questionIndex++;
-    spanMessage.textContent = " ";
-    populateQuiz();
+    if (questionIndex === questionList.length) {
+        secondsLeft = 0;
+        card2.removeAttribute("class", "visible");
+        card2.setAttribute("class", "hidden");
+        card3.setAttribute("class", "visible");
+
+    } else {
+        spanMessage.textContent = " ";
+        populateQuiz();
+    }
 }
 
 // add event listener to choices
@@ -94,19 +104,21 @@ choiceList.addEventListener("click", function (event) {
 
     if (element.innerHTML === currentObject.correct) {
         spanMessage.textContent = "Correct!";
-        console.log("Correct!");
+        score = score + 5;
         nextQuestion();
     } else {
         spanMessage.textContent = "Incorrect!";
-        console.log("incorrect");
-        // secondsLeft-10;
+       secondsLeft = secondsLeft-10;
+       if (secondsLeft < 0) {
+        secondsLeft = 0
+       } nextQuestion();
     }
 
 })
 
 //  TO DO:
 // figure out how to deduct 10 seconds from time for wrong answer
-// set up data storage for quiz scores
+
 
 // submit scores and initials
 var submitBtn = document.querySelector("#submit")
@@ -117,7 +129,7 @@ submitBtn.addEventListener("click", function (event) {
     console.log("working");
 
     var initials = initialsInput.value;
-    var score = secondsLeft;
+    
     if (initials === "") {
         alert("Initials cannot be empty");
         // tried displayMessage, but that wasn't working
@@ -126,10 +138,7 @@ submitBtn.addEventListener("click", function (event) {
         localStorage.setItem("score", score);
         window.location.href = "./highscore.html"
     }
-
-
 });
-
 
 // To end quiz:
 // if time runs out, or questionIndex > questionList.lenght
